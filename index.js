324b9fd12a06fe1fadbc338d0dafa89518496ff1
je-delray <hovax74935@ekcsoft.com>
2023-07-17 21:03:05 +0200

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+let userInput1 = prompt("Write your first string:")
+let userInput2 = prompt("Write your second string:")
+let userInput3 = prompt("Write your third string:")
+
+let result = userInput1 + " " + userInput2 + " " + userInput3
+
+alert("Result: " + result)
