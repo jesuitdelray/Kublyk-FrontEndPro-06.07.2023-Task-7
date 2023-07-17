7e62cb5d52633b007162ff9a8b6ba3e36c3b8f7e
je-delray <hovax74935@ekcsoft.com>
2023-07-17 21:05:44 +0200

    second task

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ let userInput3 = prompt("Write your third string:")
 let result = userInput1 + " " + userInput2 + " " + userInput3
 
 alert("Result: " + result)
+
+let number = prompt("Enter a five-digit number:")
+
+if (number.length !== 5) {
+    alert("You entered an incorrect number. Please enter a five-digit number.")
+} else {
+    let numberString = number.toString()
+    let digits = numberString.split("")
+    alert("Result: " + digits.join(" "))
+}
